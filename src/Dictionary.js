bedb452e54750d277e9b478f54be69eb03fb754d
shannontso <shannontan98@yahoo.com>
2021-07-29 13:52:39 +0200

    api intergration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} `);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
