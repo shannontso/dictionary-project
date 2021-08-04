727168cfccf7c4182997d3238d0bb36cc6bd0701
shannontso <shannontan98@yahoo.com>
2021-08-04 13:43:51 +0200

    added "noreferrer" to link

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,7 @@ export default function Phonetics(props) {
   return (
     <div>
       <p>{props.phonetics.text}</p>
-      <a href={props.phonetics.audio} target="_blank">
+      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
     </div>
